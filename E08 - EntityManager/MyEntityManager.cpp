1d08b1b330612bb128a45ab6a2ac23ee8da836d6
Stasha Blank <stashablank@gmail.com>
2017-11-14 10:50:20 -0500

    E08 Progress in class

--- E08 - EntityManager/MyEntityManager.cpp
+++ E08 - EntityManager/MyEntityManager.cpp
@@ -2,8 +2,13 @@
 using namespace Simplex;
 //  MyEntityManager
 MyEntityManager* MyEntityManager::m_pInstance = nullptr;
+std::vector<MyEntity> entities;
+std::vector<String> ids;
+
 void MyEntityManager::Init(void)
 {
+	entities = std::vector<MyEntity>();
+	ids = std::vector<String>();
 }
 void MyEntityManager::Release(void)
 {
@@ -18,43 +23,72 @@ MyEntityManager* MyEntityManager::GetInstance()
 }
 void MyEntityManager::ReleaseInstance()
 {
+	if (m_pInstance != nullptr)
+	{
+		delete m_pInstance;
+		m_pInstance = nullptr;
+	}
 }
 int Simplex::MyEntityManager::GetEntityIndex(String a_sUniqueID)
 {
-	return -1;
+	int index = -1;
+	for (uint i = 0; i < ids.size(); i++)
+	{
+		if (ids[i] == a_sUniqueID)
+		{
+			index = i;
+		}
+	}
+	return index;
 }
 //Accessors
 Model* Simplex::MyEntityManager::GetModel(uint a_uIndex)
 {
-	return nullptr;
+	return entities[a_uIndex].GetModel();
 }
 Model* Simplex::MyEntityManager::GetModel(String a_sUniqueID)
 {
-	return nullptr;
+	int index = GetEntityIndex(a_sUniqueID);
+
+	if (index != -1) {
+		return entities[index].GetModel();
+	}
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(uint a_uIndex)
 {
-	return nullptr;
+	return entities[a_uIndex].GetRigidBody();
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(String a_sUniqueID)
 {
-	return nullptr;
+	int index = GetEntityIndex(a_sUniqueID);
+
+	if (index != -1) {
+		return entities[index].GetRigidBody();
+	}
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(uint a_uIndex)
 {
-	return IDENTITY_M4;
+	return entities[a_uIndex].GetModelMatrix();
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(String a_sUniqueID)
 {
-	return IDENTITY_M4;
+	int index = GetEntityIndex(a_sUniqueID);
+
+	if (index != -1) {
+		return entities[index].GetModelMatrix();
+	}
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, String a_sUniqueID)
 {
+	int index = GetEntityIndex(a_sUniqueID);
 
+	if (index != -1) {
+		entities[index].SetModelMatrix(a_m4ToWorld);
+	}
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, uint a_uIndex)
 {
-
+	entities[a_uIndex].SetModelMatrix(a_m4ToWorld);
 }
 //The big 3
 MyEntityManager::MyEntityManager(){Init();}
@@ -64,15 +98,14 @@ MyEntityManager::~MyEntityManager(){Release();};
 // other methods
 void Simplex::MyEntityManager::Update(void)
 {
-
 }
 void Simplex::MyEntityManager::AddEntity(String a_sFileName, String a_sUniqueID)
 {
-
+	MyEntity newEntity = MyEntity(a_sFileName, a_sUniqueID);
+	entities.push_back(newEntity);
 }
 void Simplex::MyEntityManager::RemoveEntity(uint a_uIndex)
 {
-
 }
 void Simplex::MyEntityManager::RemoveEntity(String a_sUniqueID)
 {
